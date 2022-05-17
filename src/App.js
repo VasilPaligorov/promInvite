2ba8f2e49af62a648193939d5b3973b1c6277b50
VasilPaligorov <vpaligorov@gmail.com>
2022-05-17 15:35:59 +0300

    Added current version of the project

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import { AnimatePresence, motion } from "framer-motion";
+import useModal from "./hooks/useModal";
+import Modal from "./components/Modal";
+import { useState } from "react";
+import Confetti from 'react-confetti';
+import { useWindowSize } from '@react-hook/window-size';
+
+function App() {
+  // Modal state
+  const { modalOpen, close, open } = useModal();
+  const [img, setImg] = useState("mail.png");
+  const { width, height } = useWindowSize();
+  const [showConfetti, setShowConfetti] = useState(false);
+
+  function setOpened() {
+    setImg("openedMail.png");
+    setShowConfetti(true);  
+    open();
+  }
+
+  return (
+    <>
+      <Confetti
+        width={width}
+        height={height}
+        recycle = {showConfetti}
+      />
+      <motion.main>
+        <h1>If you aren't scared click here:</h1>
+        <motion.img src={img}
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.9 }}
+          onClick={() => setOpened()}
+          style={{
+            padding: "auto"
+          }}
+        />
+      </motion.main>
+
+      <ModalContainer>
+        {modalOpen && (
+          <Modal modalOpen={modalOpen} handleClose={close} setShowConfetti={setShowConfetti}/>
+        )}
+      </ModalContainer>
+    </>
+  );
+}
+const ModalContainer = ({ children }) => (
+
+  <AnimatePresence
+    initial={false}
+    exitBeforeEnter={true}
+  >
+    {children}
+  </AnimatePresence>
+);
+
+export default App;
---
+++ src/components/Modal/index.jsx
@@ -0,0 +1,72 @@
+import { motion } from "framer-motion";
+import Backdrop from "../Backdrop/index";
+
+const dropIn = {
+  hidden: {
+    y: "-100vh",
+    opacity: 0,
+  },
+  visible: {
+    y: "0",
+    opacity: 1,
+    transition: {
+      duration: 0.1,
+      type: "spring",
+      damping: 25,
+      stiffness: 500,
+    },
+  },
+  exit: {
+    y: "100vh",
+    opacity: 0,
+  },
+};
+
+const Modal = ({ handleClose , setShowConfetti }) => {
+
+  function closeModal() {
+    setShowConfetti(false);
+    handleClose();
+  }
+
+  return (
+    <Backdrop onClick={() => closeModal()}>
+      <motion.div
+        onClick={(e) => e.stopPropagation()}  // Prevent click from closing modal
+        className="modal orange-gradient"
+        variants={dropIn}
+        initial="hidden"
+        animate="visible"
+        exit="exit"
+      >
+        <ModalText text={"text"} />
+        <ModalButton onClick={() => closeModal()} label="Close" />
+      </motion.div>
+    </Backdrop>
+  );
+};
+
+const ModalText = () => (
+  <div className="modal-text">
+    <h3>{"text"}</h3>
+    <h5>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius laboriosam labore, totam
+      expedita voluptates tempore asperiores sequi, alias cum veritatis, minima dolor iste similique
+      eos id. Porro, culpa? Officiis, placeat?
+    </h5>
+  </div>
+);
+
+const ModalButton = ({ onClick, label }) => (
+  <motion.button
+    className="modal-button"
+    type="button"
+    whileHover={{ scale: 1.1 }}
+    whileTap={{ scale: 0.95 }}
+    onClick={onClick}
+  >
+    {label}
+  </motion.button>
+);
+
+export default Modal;
